87d4d6ac3f883931a1876314036adc14fc07b9f7
raksiarz <raksiarz@gmail.com>
2024-12-25 19:08:32 +0100

    adding noun and verb as variables

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,23 @@ import { readFileSync } from 'fs'
 import * as path from 'path'
 
 const input = readFileSync(path.resolve(__dirname, 'input.txt'), 'utf-8').split(',').map(i => +i)
-input[1] = 12
-input[2] = 2
 
 const EOF = 99 as const
 const ADD = 1 as const
 const MUL = 2 as const
+
+for(let noun = 1; noun < 99; noun++) {
+    for(let verb = 1; verb < 99; verb++) {
+        const copy = JSON.parse(JSON.stringify(input))
+        copy[1] = noun
+        copy[2] = verb 
+        const out = run(copy)
+        if(out[0] === 19690720) {
+            console.log('found noun: ', noun, ' verb: ', verb)
+            break
+        }
+    }
+}
 
 function run(input: number[]): number[] {
     let i = 0
@@ -27,4 +38,4 @@ function run(input: number[]): number[] {
     return input
 }
 
-console.log(run(input)[0])
+// console.log(run(input)[0])
