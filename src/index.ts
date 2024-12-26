7c637163233daef07f3b00114d67dedaf7bc789e
raksiarz <raksiarz@gmail.com>
2024-12-26 18:12:56 +0100

    switching to class approach

--- src/index.ts
+++ src/index.ts
@@ -15,152 +15,157 @@ const JUMP_IF_FALSE = 6 as const
 const LESS_THAN = 7 as const
 const EQ = 8 as const
 
-function getParam(index: number, mode: number) {
-    return mode ? program[index + 2] : program[program[index + 2]]
-}
-
-function parseInstruction(instruction: number) {
-    const opcode = instruction % 100
-    const p = Math.floor(instruction / 100).toString(10)
-    const params: number[] = []
-    for(let i = p.length - 1; i >= 0; i--) {
-        params.push(+p[i])
+class IntCode {
+    program: number[]
+    constructor(program: number[]) {
+        this.program = program
     }
-    
-    return { opcode, params }
-}
 
-function add(index: number, params: number[]) {
-    const param1 = params[0] ? program[index + 1] : program[program[index + 1]]
-    const param2 = params[1] ? program[index + 2] : program[program[index + 2]]
-    const memoryPos = params[2] ? index + 3 : program[index + 3]
+    parseInstruction(instruction: number) {
+        const opcode = instruction % 100
+        const p = Math.floor(instruction / 100).toString(10)
+        const params: number[] = []
+        for (let i = p.length - 1; i >= 0; i--) {
+            params.push(+p[i])
+        }
 
-    program[memoryPos] = param1 + param2
-}
-
-function mul(index: number, params: number[]) {
-    const param1 = params[0] ? program[index + 1] : program[program[index + 1]]
-    const param2 = params[1] ? program[index + 2] : program[program[index + 2]]
-    const memoryPos = params[2] ? index + 3 : program[index + 3]
-
-    program[memoryPos] = param1 * param2
-}
-
-function malloc(index: number, params: number[], input: number) {
-    const memoryPos = params[0] ? index + 1 : program[index + 1]
-
-    program[memoryPos] = input
-}
-
-function out(index: number, params: number[]) {
-    const memoryPos = params[0] ? index + 1 : program[index + 1]
-
-    console.log(program[memoryPos])
-}
-
-function jumpIfTrue(index: number, params: number[]): number | null {
-    const param1 = params[0] ? program[index + 1] : program[program[index + 1]]
-    const param2 = params[1] ? program[index + 2] : program[program[index + 2]]
-    if(param1) {
-        return param2
-    } else {
-        return null
+        return { opcode, params }
     }
-}
 
-function jumpIfFalse(index: number, params: number[]): number | null {
-    const param1 = params[0] ? program[index + 1] : program[program[index + 1]]
-    const param2 = params[1] ? program[index + 2] : program[program[index + 2]]
+    add(index: number, params: number[]) {
+        const param1 = params[0] ? this.program[index + 1] : this.program[this.program[index + 1]]
+        const param2 = params[1] ? this.program[index + 2] : this.program[this.program[index + 2]]
+        const memoryPos = params[2] ? index + 3 : this.program[index + 3]
 
-    if(!param1) {
-        return param2
-    } else {
-        return null
+        this.program[memoryPos] = param1 + param2
     }
-}
 
-function lessThan(index: number, params: number[]) {
-    const param1 = params[0] ? program[index + 1] : program[program[index + 1]]
-    const param2 = params[1] ? program[index + 2] : program[program[index + 2]]
-    const memoryPos = params[2] ? index + 3 : program[index + 3]
+    mul(index: number, params: number[]) {
+        const param1 = params[0] ? this.program[index + 1] : this.program[this.program[index + 1]]
+        const param2 = params[1] ? this.program[index + 2] : this.program[this.program[index + 2]]
+        const memoryPos = params[2] ? index + 3 : this.program[index + 3]
 
-    if(param1 < param2) {
-        program[memoryPos] = 1
-    } else {
-        program[memoryPos] = 0
+        this.program[memoryPos] = param1 * param2
     }
-}
 
-function equals(index: number, params: number[]) {
-    const param1 = params[0] ? program[index + 1] : program[program[index + 1]]
-    const param2 = params[1] ? program[index + 2] : program[program[index + 2]]
-    const memoryPos = params[2] ? index + 3 : program[index + 3]
+    malloc(index: number, params: number[], input: number) {
+        const memoryPos = params[0] ? index + 1 : this.program[index + 1]
 
-    if(param1 === param2) {
-        program[memoryPos] = 1
-    } else {
-        program[memoryPos] = 0
+        this.program[memoryPos] = input
     }
-}
 
-function run(program: number[], input: number): number[] {
-    let i = 0
-    let jumpidx = null
-    programLoop: while (i < program.length) {
-        const { opcode, params } = parseInstruction(program[i])
-        switch(opcode) {
-            case ADD:
-                add(i, params)
-                i += 4
-                break;
-            case MUL:
-                mul(i, params)
-                i += 4
-                break
-            case MALLOC:
-                malloc(i, params, input)
-                i += 2
-                break
-            case OUT:
-                out(i, params)
-                i += 2
-                break
-            case JUMP_IF_TRUE:
-                jumpidx = jumpIfTrue(i, params)
-                jumpidx ? i = jumpidx : i += 3
-                break
-            case JUMP_IF_FALSE:
-                jumpidx = jumpIfFalse(i, params)
-                jumpidx ? i = jumpidx : i += 3
-                break
-            case LESS_THAN:
-                lessThan(i, params)
-                i += 4 
-                break
-            case EQ:
-                equals(i, params)
-                i += 4 
-                break
-            case EOF:
-                console.log("Process exit")
-                break programLoop
-            default:
-                i++
-                break
+    out(index: number, params: number[]) {
+        const memoryPos = params[0] ? index + 1 : this.program[index + 1]
+
+        console.log(this.program[memoryPos])
+    }
+
+    jumpIfTrue(index: number, params: number[]): number | null {
+        const param1 = params[0] ? this.program[index + 1] : this.program[this.program[index + 1]]
+        const param2 = params[1] ? this.program[index + 2] : this.program[this.program[index + 2]]
+        if (param1) {
+            return param2
+        } else {
+            return null
         }
     }
-    
-    return program
+
+    jumpIfFalse(index: number, params: number[]): number | null {
+        const param1 = params[0] ? this.program[index + 1] : this.program[this.program[index + 1]]
+        const param2 = params[1] ? this.program[index + 2] : this.program[this.program[index + 2]]
+
+        if (!param1) {
+            return param2
+        } else {
+            return null
+        }
+    }
+
+    lessThan(index: number, params: number[]) {
+        const param1 = params[0] ? this.program[index + 1] : this.program[this.program[index + 1]]
+        const param2 = params[1] ? this.program[index + 2] : this.program[this.program[index + 2]]
+        const memoryPos = params[2] ? index + 3 : this.program[index + 3]
+
+        if (param1 < param2) {
+            this.program[memoryPos] = 1
+        } else {
+            this.program[memoryPos] = 0
+        }
+    }
+
+    equals(index: number, params: number[]) {
+        const param1 = params[0] ? this.program[index + 1] : this.program[this.program[index + 1]]
+        const param2 = params[1] ? this.program[index + 2] : this.program[this.program[index + 2]]
+        const memoryPos = params[2] ? index + 3 : this.program[index + 3]
+
+        if (param1 === param2) {
+            this.program[memoryPos] = 1
+        } else {
+            this.program[memoryPos] = 0
+        }
+    }
+
+    run(input: number): number[] {
+        let i = 0
+        let jumpidx = null
+        programLoop: while (i < this.program.length) {
+            const { opcode, params } = this.parseInstruction(this.program[i])
+            switch (opcode) {
+                case ADD:
+                    this.add(i, params)
+                    i += 4
+                    break;
+                case MUL:
+                    this.mul(i, params)
+                    i += 4
+                    break
+                case MALLOC:
+                    this.malloc(i, params, input)
+                    i += 2
+                    break
+                case OUT:
+                    this.out(i, params)
+                    i += 2
+                    break
+                case JUMP_IF_TRUE:
+                    jumpidx = this.jumpIfTrue(i, params)
+                    jumpidx ? i = jumpidx : i += 3
+                    break
+                case JUMP_IF_FALSE:
+                    jumpidx = this.jumpIfFalse(i, params)
+                    jumpidx ? i = jumpidx : i += 3
+                    break
+                case LESS_THAN:
+                    this.lessThan(i, params)
+                    i += 4
+                    break
+                case EQ:
+                    this.equals(i, params)
+                    i += 4
+                    break
+                case EOF:
+                    console.log("Process exit")
+                    break programLoop
+                default:
+                    i++
+                    break
+            }
+        }
+
+        return this.program
+    }
 }
 
+
 const rl = readline.createInterface({ input: stdin, output: stdout })
+const proc = new IntCode(program)
 
 async function getInput() {
     console.log('Please provide ID:\n1. Test')
     const inp = await rl.question('>> ')
     if(inp === ':q') process.exit(0);
 
-    run(program, +inp)
+    proc.run(+inp)
     process.exit(0)
 }
 
